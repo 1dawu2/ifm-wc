4c06688e5ebf76c1801f76ab49262f99bfaf6af5
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-19 12:34:16 +0100

    update

--- src/components/chatGPT.js
+++ src/components/chatGPT.js
@@ -62,11 +62,6 @@ export default class ChatGPT extends HTMLElement {
     render() {
         const { shadowRoot } = this;
 
-        const { cssContent, htmlContent } = this.htmlToElement(html);
-        shadowRoot.innerHTML = '';
-        shadowRoot.appendChild(cssContent)
-        shadowRoot.appendChild(htmlContent)
-
         // create chatbox element
         const chatbox = document.createElement('div');
         chatbox.id = 'chatbox';
@@ -301,12 +296,5 @@ export default class ChatGPT extends HTMLElement {
         } catch (error) {
             console.error(error);
         }
-    }
-
-    htmlToElement(html) {
-        var template = document.createElement('template');
-        html = html.trim(); // Never return a text node of whitespace as the result
-        template.innerHTML = html;
-        return { cssContent: template.content.firstChild, htmlContent: template.content.lastChild };
     }
 }
