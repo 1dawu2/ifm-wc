5f6ccdaac7341074106b0e876892143e33170e59
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-20 00:24:25 +0100

    update

--- src/components/chatGPT.js
+++ src/components/chatGPT.js
@@ -262,14 +262,11 @@ export default class ChatGPT extends HTMLElement {
             apiKey: this._export_settings.apiSecret,
         });
         const openai = new OpenAIApi(configuration);
-
-        const response = await openai.createCompletion("text-davinci-002", {
-            prompt: message,
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `"${message}"`,
             temperature: 0,
-            max_tokens: 256,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
+            max_tokens: 7,
         });
 
         console.log(response.choices[0].text);
