2ed2aeaf7c0dc4449244133f5025a55de8bdd85a
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-22 19:40:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -330,7 +330,7 @@
     let storyModel = documentContext.get("sap.fpa.story.getstorymodel");
     console.log("Story Model:");
     console.log(storyModel);
-    let storyService =  sap.fpa.ui.infra.common.getContext().get("sap.fpa.story.StoryService");
+    let storyService = sap.fpa.ui.infra.common.getContext();
     console.log("Story Service");
     console.log(storyService);
     let entityService = documentContext.get("sap.fpa.bi.entityService");
