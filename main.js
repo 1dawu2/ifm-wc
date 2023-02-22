427cb328016773f23e11dbbb0ce080618b87e2bb
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-22 14:03:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -278,6 +278,9 @@
 
     if (sap.fpa.story) {
       console.log(sap.fpa.story);
+      var story = sap.fpa.story.getStoryById("59A395046F3F8A41401B0B1C28FD787D");
+      console.log("Story details:");
+      console.log(story);
     }
 
     if (sap.fpa.ui.story) {
