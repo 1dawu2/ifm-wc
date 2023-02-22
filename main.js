d89d5788e8fdb86b4499b1747a8a7574d763ccfd
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-22 15:06:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -149,11 +149,40 @@
                 sortProperty: "Created",
                 filterProperty: "Created",
               }));
-              
+
+              // create the paginator
+              var oPaginator = new sap.ui.commons.Paginator({
+                numberOfPages: 1, // set the number of pages to 1 initially
+                currentPage: 1, // set the current page to 1 initially
+                visible: false, // hide the paginator initially
+                page: function (oEvent) {
+                  // update the table data when the page changes
+                  var iPage = oEvent.getParameter("page");
+                  oModel.setData(getDataForPage(iPage));
+                  oTable.setFirstVisibleRow(0);
+                }
+              });
+
               oTable.setModel(oModel, "artifact");
               oTable.bindRows("artifact>/");
+              oPaginator.bindAggregation("content", "/", function (sId, oContext) {
+                // create a new button for each page of data
+                var iIndex = oContext.getProperty();
+                return new sap.ui.commons.Button({
+                  text: iIndex + 1,
+                  press: function () {
+                    oPaginator.setCurrentPage(iIndex + 1);
+                  }
+                });
+              });
 
-              this.oPanel.addContent(oTable);
+              // add the table and paginator to the page
+              var oPage = new sap.m.Page({
+                title: "My Page",
+                content: [oTable, oPaginator]
+              });
+
+              this.oPanel.addContent(oPage);
             }
 
           });
@@ -261,6 +290,9 @@
     let storyModel = documentContext.get("sap.fpa.story.getstorymodel");
     console.log("Story Model:");
     console.log(storyModel);
+    let story = documentContext.get("sap.fpa.story.getStoryById");
+    console.log("Story");
+    console.log(story);
     let entityService = documentContext.get("sap.fpa.bi.entityService");
     console.log("Entity Service:");
     console.log(entityService);
@@ -301,16 +333,12 @@
 
   function convertSACArtifact() {
 
-    if (sap.fpa) {
-      console.log(sap.fpa.story);
-      var story = sap.fpa.story.getStoryById("59A395046F3F8A41401B0B1C28FD787D");
-      console.log("Story details:");
-      console.log(story);
-    }
-
-    if (sap.fpa.ui.story) {
-      console.log(sap.fpa.ui.story);
-    }
+    // if (sap.fpa) {
+    //   console.log(sap.fpa.story);
+    //   var story = sap.fpa.story.getStoryById("59A395046F3F8A41401B0B1C28FD787D");
+    //   console.log("Story details:");
+    //   console.log(story);
+    // }
 
     // var aUnsupportedFeatures = sap.fpa.story.optimizedModeFeaturesValidator.getUnsupportedFeatures();
     // for (var i = 0; i < aUnsupportedFeatures.length; i++) {
