e7b2f9da6096036dea69aafaa39daf9ff1ff80bd
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-26 13:25:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -97,7 +97,7 @@
                       </m:layoutData>
                     </m:Button>
                   </m:OverflowToolbar>	
-                  <m:Panel id="oPanel" height="100%" width="auto" class="sapUiResponsiveMargin"/>
+                  <m:Panel id="oPanel"/>
                   <m:OverflowToolbar>
                     <m:OverflowToolbarButton tooltip="Favorite" text="Favorite" icon="sap-icon://favorite"/>
                   </m:OverflowToolbar>
