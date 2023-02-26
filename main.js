c2a4b07881799256f0ac7063cc27bcce31df6e69
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-26 13:27:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -97,7 +97,7 @@
                       </m:layoutData>
                     </m:Button>
                   </m:OverflowToolbar>	
-                  <m:Panel id="oPanel"/>
+                  <m:content id="oPanel"/>
                   <m:OverflowToolbar>
                     <m:OverflowToolbarButton tooltip="Favorite" text="Favorite" icon="sap-icon://favorite"/>
                   </m:OverflowToolbar>
