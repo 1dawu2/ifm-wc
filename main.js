6187d3c836111ee6bb1f76579d85187ee1f06356
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-02-26 14:15:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -349,7 +349,7 @@
               const dt = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "yyyy-MM-ddTHH:mm:ss" })
               oTable.addColumn(new sap.ui.table.Column({
                 label: new sap.ui.commons.Label({ text: "Created" }),
-                template: new sap.ui.commons.TextView({ text: "dt.format(dt.parse({artifact>created}))" }),
+                template: new sap.ui.commons.TextView({ text: `"${dt.format(dt.parse("{artifact>created}"))}"` }),
                 sortProperty: "Created",
                 filterProperty: "Created",
               }));
