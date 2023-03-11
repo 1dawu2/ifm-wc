e9e8c5314b684e260d717dda75a07d0caf39e593
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-03-11 15:51:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -279,12 +279,7 @@
               if (sKey === "Activity") {
                 var oModelActivities = new sap.ui.model.json.JSONModel();
                 var cHeader = { "DataServiceVersion": "2.0", "Accept": "*/*" };
-                oModelActivities.loadData("https://infomotion1.eu10.hanacloudservices.cloud.sap/api/v1/audit/activities/exportActivities?sortDescending=true&sortKey=TIMESTAMP&pageIndex=1&pageSize=100000&csvName=activities", oData, true, "POST", null, false, cHeader);
-
-                oModelActivities.attachRequestCompleted(function (oEvent) {
-                  var oData = oEvent.getSource().oData;
-                  console.log(oData);
-                });
+                oModelActivities.loadData("https://infomotion1.eu10.hanacloudservices.cloud.sap/api/v1/audit/activities/exportActivities?sortDescending=true&sortKey=TIMESTAMP&pageIndex=1&pageSize=100&csvName=activities", null, true, "GET", null, false, cHeaders);
 
               }
 
@@ -301,7 +296,7 @@
                   }),
                   customHeader: new sap.m.Bar({
                     contentMiddle: [
-                      new Title({
+                      new sap.m.Title({
                         text: "Story Übersicht",
                       })
                     ]
