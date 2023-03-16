a2ea95c72ec2bbbfee13a445a68de1185bb7c954
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-03-16 12:47:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -643,7 +643,7 @@
                         console.log("Final pending: ", myPromise.isPending());
                       });
 
-                    };
+                    }
                   }
                 }),
                 sortProperty: "id",
