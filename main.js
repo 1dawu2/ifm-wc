4178e3f506b1693f1363e8c9d3bb052cf9ba12c8
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-03-25 15:39:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -589,7 +589,7 @@
               oModel.attachRequestCompleted(function (oEvent) {
                 console.log("json model");
                 console.log(oModel);
-                var propID = this.getProperty("id");
+                var propID = this.getProperty("/id");
                 console.log("property");
                 console.log(propID);
                 var tableData = this.getData();
