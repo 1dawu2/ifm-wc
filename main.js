4fc4598d81c5ecc7d6845b9caf33aea7556e20c2
1dawu2 <94387071+1dawu2@users.noreply.github.com>
2023-03-27 00:21:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -813,7 +813,7 @@
                 template: new sap.ui.commons.ListBox({
                   items: {
                     path: "artifact>models/",
-                    template: new sap.ui.core.ListItem({ text: "{artifact>description}" })
+                    template: new sap.ui.core.ListItem({ text: "{artifact>id}" })
                   },
                 })
               }));
